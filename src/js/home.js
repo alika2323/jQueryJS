52030787870a13337fbb5ef4bb867d11131be870
Nallely Rodriguez <nallely.rf89@gmail.com>
2018-11-21 12:57:55 -0600

    Creando funcion asincrona getDataMovies

--- src/js/home.js
+++ src/js/home.js
@@ -1,92 +1,15 @@
-// /* EJEMPLO DE PROMESA BASICO*/
-// const status=1;
-// const getUser = new Promise(function(salioBien, salioMal){
-// 	if (status==1) {
-// 		salioBien('USER1: OK OK');
-// 	} else {
-// 		salioMal('USER1: Se acabo el tiempo');
-// 	}
-// 	// setTimeout(function(){
-// 	// 	salioMal();
-// 	// },3000);
-// })
+(async function load(){
+	async function getDataMovies(url){
+		const response = await fetch(url)
+		const datos = await response.json()
+		return datos;
+	}
 
+	const actionList = await getDataMovies('https://yts.am/api/v2/list_movies.json?genre=action');
+	const dramaList = await getDataMovies('https://yts.am/api/v2/list_movies.json?genre=drama');
+	const animationList = await getDataMovies('https://yts.am/api/v2/list_movies.json?genre=animation');
 
-// getUser
-// .then(function(msj){
-// 	console.log(msj);
-// })
-// .catch(function(msj){
-// 	console.log(msj);
-// });
-
-
-
-// /* EJEMPLO DE EJECUCION DE VARIAS PROMESAS*/
-// const status2=1;
-// const status3=1;
-// const getUser2 = new Promise(function(salioBien, salioMal){
-// 	if (status2==1) {
-// 		salioBien('USER2:OK OK');
-// 	} else {
-// 		salioMal('USER2:Se acabo el tiempo');
-// 	}
-// })
-
-// const getUser3 = new Promise(function(salioBien, salioMal){
-// 	if (status3==1) {
-// 		salioBien('USER3:OK OK');
-// 	} else {
-// 		salioMal('USER3:Se acabo el tiempo');
-// 	}
-// })
-
-
-// Promise.all([
-// 	getUser2,
-// 	getUser3
-// 	])
-// .then(function(msj){
-// 	console.log(msj);
-// })
-// .catch(function(msj){
-// 	console.log(msj);
-// })
-
-
-// /* TRAER DATOS DE UN SERVIDOR CON AJAX  (JQUERY )*/
-// $.ajax('https://randomuser.me/api/',
-// {
-// 	method: 'GET',
-// 	success: function(data){
-// 		console.log("AJAX->"+data.results[0].name.first);
-// 	},
-// 	error: function(error){
-// 		console.log("AJAX->"+error.statusText);
-// 	}
-// })
-
-
-
-//  TRAER DATOS DE UN SERVIDOR CON FETCH (JS VANILLA )
-// fetch('https://randomuser.me/api/')
-// .then(function(respuesta){
-// 	return respuesta.json() ;
-// })
-// .then(function(data){
-// 	console.log("JS->"+data.results[0].name.first);
-// })
-
-// .catch(function(error){
-// 	console.log("JS->"+error);
-// });
-
-
-
-/* FUNCIONES ASINCRONAS */
-(async function user(){
-	const respuesta = await fetch('https://randomuser.me/api/');
-	const data = await respuesta.json();
-	console.log("ASYNC->"+data.results[0].name.first);
+	console.log(actionList.data.movies);
+	console.log(dramaList.data.movies);
+	console.log(animationList.data.movies);
 })()
-
