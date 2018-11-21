640313205cf6a156c0cb6089345fa4450c0138ee
Nallely Rodriguez <nallely.rf89@gmail.com>
2018-11-21 11:09:32 -0600

    Ejemplo de promesas

--- src/js/home.js
+++ src/js/home.js
@@ -1,8 +1,55 @@
-console.log('hola mundo!');
-const noCambia = "Leonidas";
+/* EJEMPLO DE PROMESA BASICO*/
+const status=1;
+const getUser = new Promise(function(salioBien, salioMal){
+	if (status==1) {
+		salioBien('USER1: OK OK');
+	} else {
+		salioMal('USER1: Se acabo el tiempo');
+	}
+	// setTimeout(function(){
+	// 	salioMal();
+	// },3000);
+})
 
-let cambia = "@LeonidasEsteban"
 
-function cambiarNombre(nuevoNombre) {
-  cambia = nuevoNombre
-}
+getUser
+.then(function(msj){
+	console.log(msj);
+})
+.catch(function(msj){
+	console.log(msj);
+});
+
+
+
+/* EJEMPLO DE EJECUCION DE VARIAS PROMESAS*/
+const status2=1;
+const status3=1;
+const getUser2 = new Promise(function(salioBien, salioMal){
+	if (status2==1) {
+		salioBien('USER2:OK OK');
+	} else {
+		salioMal('USER2:Se acabo el tiempo');
+	}
+})
+
+const getUser3 = new Promise(function(salioBien, salioMal){
+	if (status3==1) {
+		salioBien('USER3:OK OK');
+	} else {
+		salioMal('USER3:Se acabo el tiempo');
+	}
+})
+
+
+Promise.all([
+	getUser2,
+	getUser3
+	])
+.then(function(msj){
+	console.log(msj);
+})
+.catch(function(msj){
+	console.log(msj);
+})
+
