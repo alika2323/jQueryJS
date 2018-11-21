84780bcf31d997aa7f98b6be3b2d98dafe3e0161
Nallely Rodriguez <nallely.rf89@gmail.com>
2018-11-21 13:23:17 -0600

    Agregando selectores a JS

--- src/js/home.js
+++ src/js/home.js
@@ -1,3 +1,4 @@
+const prueba=1;
 (async function load(){
 	async function getDataMovies(url){
 		const response = await fetch(url)
@@ -9,7 +10,26 @@
 	const dramaList = await getDataMovies('https://yts.am/api/v2/list_movies.json?genre=drama');
 	const animationList = await getDataMovies('https://yts.am/api/v2/list_movies.json?genre=animation');
 
-	console.log(actionList.data.movies);
-	console.log(dramaList.data.movies);
-	console.log(animationList.data.movies);
+
+
+
+	const $actionListContainer = document.getElementById('action');
+	const $dramaListContainer = document.getElementById('drama');
+	const $animationListContainer = document.getElementById('animation');
+
+	const $featuringContainer = document.getElementById('featuring');
+	const $form = document.getElementById('form');
+	const $home = document.getElementById('home');
+
+
+	const $modal = document.getElementById('modal');
+	const $overlay = document.getElementById('overlay');
+	const $hideModal = document.getElementById('hide-modal');
+
+
+	/* selectores del modal */
+	const $modalImage = $modal.querySelector('img');
+	const $modalTitle = $modal.querySelector('h1');
+	const $modalDescription = $modal.querySelector('p');
+
 })()
