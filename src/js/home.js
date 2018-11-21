52442f2d1303fbab5ee1782655916e2f8d2991f9
Nallely Rodriguez <nallely.rf89@gmail.com>
2018-11-21 15:00:54 -0600

    Creando funcion videoItemTemplate (template para video)

--- src/js/home.js
+++ src/js/home.js
@@ -1,18 +1,8 @@
 const prueba=1;
 (async function load(){
-	async function getDataMovies(url){
-		const response = await fetch(url)
-		const datos = await response.json()
-		return datos;
-	}
 
-	const actionList = await getDataMovies('https://yts.am/api/v2/list_movies.json?genre=action');
-	const dramaList = await getDataMovies('https://yts.am/api/v2/list_movies.json?genre=drama');
-	const animationList = await getDataMovies('https://yts.am/api/v2/list_movies.json?genre=animation');
-
-
-
-
+	/* selectores */
+	
 	const $actionListContainer = document.getElementById('action');
 	const $dramaListContainer = document.getElementById('drama');
 	const $animationListContainer = document.getElementById('animation');
@@ -27,9 +17,50 @@ const prueba=1;
 	const $hideModal = document.getElementById('hide-modal');
 
 
-	/* selectores del modal */
 	const $modalImage = $modal.querySelector('img');
 	const $modalTitle = $modal.querySelector('h1');
 	const $modalDescription = $modal.querySelector('p');
+
+
+
+	/* listas de peliculas */
+	const actionList = await getDataMovies('https://yts.am/api/v2/list_movies.json?genre=action');
+	const dramaList = await getDataMovies('https://yts.am/api/v2/list_movies.json?genre=drama');
+	const animationList = await getDataMovies('https://yts.am/api/v2/list_movies.json?genre=animation');
+
+
+
+
+
+
+
+
+
+	actionList.data.movies.forEach((movie)=>{
+		console.log(videoItemTemplate(movie))
+	})
+
+
+
+	/* funciones */
+	async function getDataMovies(url){
+		const response = await fetch(url)
+		const datos = await response.json()
+		return datos;
+	}
+
+	function videoItemTemplate(movie){
+		return(
+			`<div class="primaryPlaylistItem">
+			<div class="primaryPlaylistItem-image">
+			<img src="${movie.medium_cover_image}">
+			</div>
+			<h4 class="primaryPlaylistItem-title">
+			${ movie.title}
+			</h4>
+			</div>`
+			)
+	}
+
 
 })()
